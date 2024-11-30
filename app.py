2ae1f35e8764f89ee06b385fc551a514757affe7
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-11-30 06:38:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ if jogos_dia_file:
     ha_mais_um_jogos = jogos_dia_validos[
         jogos_dia_validos['Time_Fora'].apply(
             lambda x: any(fuzz.partial_ratio(x, equipe) > 80 for equipe in ha_mais_um_filtrados['Equipe'])
-        ) & (jogos_dia_validos['Home'] >= 2.40) & (jogos_dia_validos['Away'] >= 2.4)
+        ) & (jogos_dia_validos['Home'] >= 3.40) & (jogos_dia_validos['Away'] >= 2.4)
     ]
     st.dataframe(ha_mais_um_jogos)
 
