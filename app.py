c58b37c63427a9b9e6651c5b107971c33ed15eee
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-12-01 08:42:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -112,6 +112,21 @@ if jogos_dia_file:
     ]
     st.dataframe(ha_pos_jogos)
 
+    # Análise: HA +0.25 Away
+    st.subheader("HA +0.25 Away")
+    melhores_home_filtrados = melhores_casa[melhores_casa['W'] >= 3]
+    melhores_away_filtrados = melhores_fora[melhores_fora['W'] >= 3]
+    ha_25_away = jogos_dia_validos[
+        jogos_dia_validos['Time_Fora'].apply(
+            lambda x: any(fuzz.partial_ratio(x, equipe) > 80 for equipe in melhores_away_filtrados['Equipe'])
+        )
+        & (jogos_dia_validos['Home'] >= 1.80) & (jogos_dia_validos['Away'] >= 1.8)
+        & jogos_dia_validos['Time_Casa'].apply(
+            lambda x: any(fuzz.partial_ratio(x, equipe) > 80 for equipe in melhores_home_filtrados['Equipe'])
+        )
+    ]
+    st.dataframe(ha_25_away)
+
     # Análise: HA +1
       
     st.subheader("HA +1")
