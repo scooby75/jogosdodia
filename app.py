150faa6bf1ee701682f9d54bf93814c35250a341
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-12-10 12:54:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,39 +9,23 @@ url_equipes_fora = "https://raw.githubusercontent.com/scooby75/jogosdodia/refs/h
 df_casa = pd.read_csv(url_equipes_casa)
 df_fora = pd.read_csv(url_equipes_fora)
 
-# Verificar os tipos de dados das colunas
-st.write("Tipos de dados das colunas de equipes_casa:", df_casa.dtypes)
-st.write("Tipos de dados das colunas de equipes_fora:", df_fora.dtypes)
-
-# Concatenar os DataFrames
-try:
-    df = pd.concat([df_casa, df_fora], ignore_index=True)
-    st.write("DataFrame concatenado com sucesso!")
-except Exception as e:
-    st.error(f"Ocorreu um erro ao concatenar os DataFrames: {e}")
-
-# Verificar valores nulos no DataFrame concatenado
-st.write("Valores nulos no DataFrame:", df.isnull().sum())
-
-# Substituir valores nulos por um valor padrão, se necessário
-df.fillna({'Odd_Justa_MO': 0, 'Odd_Justa_HA': 0, 'GD_Home': 0, 'PIH': 0, 'PIH_HA': 0}, inplace=True)
-
-# Seleção de equipe da casa e equipe de fora
-equipe_casa = st.selectbox("Selecione a Equipe da Casa", df['Equipe'].unique())
-equipe_fora = st.selectbox("Selecione a Equipe de Fora", df['Equipe'].unique())
+# Seleção de equipe da casa
+equipe_casa = st.selectbox("Selecione a Equipe da Casa", df_casa['Equipe'].unique())
 
 # Exibir os dados da equipe da casa
 st.write(f"Dados da equipe da casa {equipe_casa}:")
-df_casa_selecionada = df[df['Equipe'] == equipe_casa]
+df_casa_selecionada = df_casa[df_casa['Equipe'] == equipe_casa]
 st.write(df_casa_selecionada)
+
+# Seleção de equipe de fora
+equipe_fora = st.selectbox("Selecione a Equipe de Fora", df_fora['Equipe'].unique())
 
 # Exibir os dados da equipe de fora
 st.write(f"Dados da equipe de fora {equipe_fora}:")
-df_fora_selecionada = df[df['Equipe'] == equipe_fora]
+df_fora_selecionada = df_fora[df_fora['Equipe'] == equipe_fora]
 st.write(df_fora_selecionada)
 
 # Comparação de algumas estatísticas entre as equipes
-# Exemplo: Comparando gols (GF) e pontos (Pts_Home ou Pts_Away)
 compara_gols = st.checkbox("Comparar gols (GF) das equipes")
 
 if compara_gols:
