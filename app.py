f1bfeb44671388b96a03a0e5cc0d1d8059fd0779
scooby75 <45230721+scooby75@users.noreply.github.com>
2025-04-30 05:54:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -657,7 +657,7 @@ with tabs[0]:
             # Texto de análise
             analise_home = f"""
             ### 🏠 {equipe_home} (Casa)
-            O time da casa **{equipe_home}** apresenta um **{desempenho_home} desempenho** como mandante, com uma frequência de **{gf_avg_home:.2f} gols** por partida e uma média de pontos por jogo (PPG) de **{ppg_home:.2f}**. 
+            O time da casa **{equipe_home}** apresenta um **{desempenho_home} desempenho** como mandante, com uma frequência de **{gf_avg_home:.2f} gols** por partida, média de pontos por jogo (PPG) de **{ppg_home:.2f}** e um total de **{gp_home} partidas** jogadas em casa.
             """
     
             if home_fg_data is not None:
